7c330054ff01f681dee787f072be9814c2ef8fa7
PedroCirotto <pedro@peakonedev.com>
2024-09-18 09:02:19 -0300

    footer cadastro

--- src/pages/Cadastros/index.jsx
+++ src/pages/Cadastros/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Cards from "../../components/Cards";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 function Cadastros() {
   return (
     <>
       <Header />
       <Cards />
+      <Footer />
     </>
   );
 }
